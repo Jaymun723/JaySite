12bac6281f7ca3f3547822bb8e0c229406f37aac
Jaymun723 <jaymun723@yahoo.com>
2017-09-09 14:09:56 +0200

    Added little things

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function (req, res) {
   res.render('index', {title: 'Accueil', desc: '"Le meilleur site du monde" - Jaymun723'})
 })
 
+router.get('/contact', function (req, res)  {
+  res.render('contact', {title: 'Contact', desc: 'Me contacter'})
+})
+
 router.get('/404', function (req, res) {
   res.render('404')
 })
--- routes/trucs.js
+++ routes/trucs.js
@@ -7,7 +7,11 @@ router.get('/', function (req, res) {
 })
 
 router.get('/ez', function (req, res) {
-  res.render('ez', {title: 'Ez'})
+  res.render('ez', {title: 'Ez', desc: 'Ez in all is simplicity....'})
+})
+
+router.get('/bot', function (req, res) {
+  res.render('bot', {title: 'Tutoriel pour faire un bot discord.', desc: 'Un petit tutoriel pour faire facilement des bots discord en utilisant ContainScord !'})
 })
 
 module.exports = router
